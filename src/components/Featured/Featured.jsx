1e54f8266775167f57ec12e65eb10a8722b251fb
NirzanMalla <nirzanmalla@gmail.com>
2023-08-17 10:08:34 +0545

    Worked on the featured page

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -7,14 +7,14 @@ const Featured = () => {
     <div className='featured'>
       <img 
       width='100%'
-      src="https://images.alphacoders.com/128/1289451.jpg" alt="Poster"
+      src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiNh4rDMFFvBFxzjNq6yQ9lRIqJH-mEn3yXERyZ88VeIE-KC19AK8xta3snVkiPAWP696uBzYxI3jW8l42tHb2BQF6HTSFJX-CbWjwOSaN-PUiXQzxx17NcxMteECV6LEWqhAnGjYsXNkJGf6zlwKWnitvZQO7lW25nHAA_9n3pbEce2ANW04eNijF0bw/s1600/WEDNESDAY-ADDAMS-1022023.png" alt="Poster"
       />
     <div className="info">
-    {/* <img 
-      src="https://w7.pngwing.com/pngs/387/687/png-transparent-logo-game-of-thrones-television-text-logo.png" alt="Poster"
-      /> */}
+    <img 
+      src="https://logos-world.net/wp-content/uploads/2022/12/Logo-Wednesday.png" alt="Poster"
+      />
       <span className="desc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quaerat maiores necessitatibus aliquid nobis nisi! Nam ipsam quam eaque alias minima corrupti, eveniet nulla excepturi.
+      Smart, sarcastic and a little dead inside, Wednesday Addams investigates a murder spree while making new friends — and foes — at Nevermore Academy.
       </span>
       <div className="buttons">
         <button className="play">
